4a9301f55780ee41c1a2b1e42ba996e7e063ba86
adqt0019 <alesandro.balani@adiq.com.br>
2020-04-25 18:15:08 -0300

    função de calcular

--- src/App.js
+++ src/App.js
@@ -17,25 +17,63 @@ import Display from './components/Display'
 
 export default class App extends Component {
   state = {
-    displayValue: '0'
+    displayValue: '0',
+    displayHistory: '',
+    atualOperation: ''
   }
 
   addDigit = n => {
-    this.setState({ displayValue: n })
+    this.setState({ 
+      displayValue: this.state.displayValue === '0' ? n : this.state.displayValue + n
+    })
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: '0' })
+    this.setState({ displayValue: '0', displayHistory: '', atualOperation: '' })
   }
 
-  setOperation = operation => {
+  setOperation = operation => {    
+    this.setState({ 
+      displayHistory: this.state.atualOperation === '' ? this.state.displayValue + operation : this.calcula() + operation,
+      displayValue: '0',
+      atualOperation: operation
+    })
+  }
 
+  operaIgual = () => {
+    this.setState({
+      displayHistory: this.state.atualOperation === '' ? this.state.displayHistory : '',
+      displayValue: this.state.atualOperation === '' ? this.state.displayValue : this.calcula(),
+      atualOperation: ''
+    })
+  }
+
+  calcula = () => {
+    let num1 = this.state.displayHistory.substr(0, this.state.displayHistory.indexOf(this.state.atualOperation))    
+    let retorno = '0'
+    switch (this.state.atualOperation) {
+      case '+': 
+        retorno =  num1 + this.state.displayValue
+        break;
+      case '-': 
+        retorno = num1 - this.state.displayValue
+        break
+      case '*': 
+        retorno = num1 * this.state.displayValue
+        break
+      case '/': 
+        retorno = num1 / this.state.displayValue
+        break
+      default: retorno = 'ERROR'
+      break
+    }
+    return retorno
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Display value={this.state.displayValue} />
+        <Display value={this.state.displayValue} history={this.state.displayHistory} />
         <View style={styles.buttons}>
           <Button label='AC' tripleButton onClick={this.clearMemory} />
           <Button label='/' operation onClick={this.setOperation} />
@@ -53,7 +91,7 @@ export default class App extends Component {
           <Button label='+' operation onClick={this.setOperation}/>
           <Button label='0' doubleButton  onClick={this.addDigit}/>
           <Button label='.' onClick={this.addDigit}/>
-          <Button label='=' operation onClick={this.setOperation}/>
+          <Button label='=' operation onClick={this.operaIgual}/>
         </View>
       </View>
     )
--- src/components/Display.js
+++ src/components/Display.js
@@ -7,7 +7,7 @@ import {
 
 const styles = StyleSheet.create({
     display: {
-        flex: 1,
+        flex: 2,
         padding: 20,
         justifyContent: 'center',
         backgroundColor: 'rgba(0,0,0,0.6)',
@@ -17,10 +17,16 @@ const styles = StyleSheet.create({
         fontSize: 60,
         color: '#fff',
     },
+    displayHistory: {
+        fontSize: 30,
+        color: '#fff',
+    }
 })
 
 export default props => 
     <View style={styles.display}>
+        <Text style={styles.displayHistory}
+            numberOfLines={1}>{props.history}</Text>
         <Text style={styles.displayValue}
             numberOfLines={1}>{props.value}</Text>
     </View>
